e4d3795b705adeeaf5ca949a4e214bf98ee046bf
HarmonicTons <roncin.thomas.l.l@gmail.com>
2017-01-20 11:59:51 +0100

    Update applyModel.js

--- applyModel.js
+++ applyModel.js
@@ -5,73 +5,91 @@
  * Applicable formats:
  *      - "export-ignore": the property will not be exported in the XML,
  *      - "export-toAttribute": the property will be an attribute of its parent in the XML,
- *      - "export-ordered" & "export-order": the properties of this object will be in a specified
- *          ordered in the XML.
+ *      - "export-order": the properties will be in a specified ordered in the XML.
  *
- * <TRO> <07/12/2016>
+ * Created by troncin on 07/12/2016.
  */
 
 'use strict';
 
 // dependencies
-var _ = require('lodash');
+const _ = require('lodash');
 
 /**
- * module function
+ * Apply a model to an object
+ * @param data object to modify
+ * @param model model to apply
  */
-function applyModel(obj, model, parent, key) {
+function applyModel(data, model) {
+    try {
 
-    // Apply to items of arrays
-    if (model.type === "array") {
-        _.forEach(obj, (item, index) => {
-            applyModel(item, model.items, obj, index);
-        });
-    }
-    // Apply to properties of objects
-    else if (model.type === "object" && model.properties) {
+        // Apply to items of arrays
+        if (model.type === "array") {
+            return _.map(data, item => applyModel(item, model.items));
+        }
 
-        // remove properties from the data that are not present in the model
-        _.forEach(obj, (val, prop) => {
-            if (!model.properties.hasOwnProperty(prop)) {
-                delete obj[prop];
-            }
-        });
+        // Apply to properties of objects or maps
+        else if (model.type === "object" && model.properties) {
 
-        // for each property in the model
-        _.forEach(model.properties, (submodel, prop) => {
-            // properties to ignore
-            if (submodel["export-ignore"]) {
-                delete obj[prop];
-                return;
-            }
-
-            // create properties from the model that are missing in the data
-            if (!obj.hasOwnProperty(prop)) {
-                obj[prop] = {};
-            }
-
-            // transform a property into an attribute
-            else if (submodel["export-toAttribute"]) {
-                obj['@' + prop] = {};
-                obj['@' + prop][prop] = obj[prop];
-                delete obj[prop];
-            }
-
-            applyModel(obj[prop], model.properties[prop], obj, prop);
-        });
-
-        // order properties if needed
-        if (model["export-ordered"]) {
-            var map = new Map();
-            _.forEach(obj, (val, key) => {
+            let map = new Map();
+            _.forEach(data, (val, key) => {
                 map.set(key, val);
             });
-            parent[key] = new Map([...map.entries()].sort((a, b) => {
-                if (!model.properties[a[0]] || !model.properties[a[0]]["export-order"]) return 999;
-                if (!model.properties[b[0]] || !model.properties[b[0]]["export-order"]) return -999;
-                return model.properties[a[0]]["export-order"] - model.properties[b[0]]["export-order"];
-            }));
+
+            // remove properties from the data that are not present in the model
+            _.forEach(map, (val, prop) => {
+                if (!model.properties.hasOwnProperty(prop)) {
+                    map.delete(prop);
+                }
+            });
+
+            // for each property in the model
+            var isOrdered = false;
+            _.forEach(model.properties, (submodel, prop) => {
+                //check if is ordered
+                if (submodel["export-order"]) {
+                    isOrdered = true;
+                }
+
+                // properties to ignore
+                if (submodel["export-ignore"]) {
+                    map.delete(prop);
+                    return;
+                }
+
+                // create properties from the model that are missing in the data
+                if (typeof map.get(prop) === 'undefined') {
+                    map.set(prop, {});
+                }
+
+                // transform a property into an attribute
+                else if (submodel["export-toAttribute"]) {
+                    let subobj = {}
+                    subobj[prop] = map.get(prop);
+                    map.set('@' + prop, subobj);
+                    map.delete(prop);
+                }
+
+                // apply model to children
+                map.set(prop, applyModel(map.get(prop), model.properties[prop]));
+            });
+
+            // order properties if needed
+            if (isOrdered) {
+                return new Map([...map.entries()].sort((a, b) => {
+                    if (!model.properties[a[0]] || !model.properties[a[0]]["export-order"]) return 999;
+                    if (!model.properties[b[0]] || !model.properties[b[0]]["export-order"]) return -999;
+                    return model.properties[a[0]]["export-order"] - model.properties[b[0]]["export-order"];
+                }));
+            }
+
+            return map;
         }
+
+        return data;
+
+    } catch (error) {
+        console.error(`ApplyModel Error: \n\t Data: ${JSON.stringify(data)}, \n\t Model: ${JSON.stringify(model)}, \n\t Error: ${error}.`);
     }
 }
 
